78498fb64020d624b46b6379613ec11bb520d45a
tabbbyzzxc <ipz221_mms@student.ztu.edu.ua>
2022-11-24 17:36:11 +0100

    Add project files.

---
+++ HowTo/Models/AddViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace HowTo.Models
+{
+    public class AddViewModel
+    {
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
---
+++ HowTo/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HowTo.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
